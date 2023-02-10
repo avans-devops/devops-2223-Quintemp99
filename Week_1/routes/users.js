20cf36503915f65582c7a3d2d50073305e23ea9d
Quin-Tempelaars <q.tempelaars@gmail.com>
2023-02-10 14:30:42 +0100

    Changed error text

--- Week_1/routes/users.js
+++ Week_1/routes/users.js
@@ -20,7 +20,7 @@ router.route('/').get((req, res) => {
       db.collection("users")
         .insertOne(req.body, function (err, result) {
           if (err) {
-            res.status(400).send("Error inserting je moeder!");
+            res.status(400).send("Error inserting user!");
           } else {
             console.log('id', result.insertedId);
             res.status(204).send();
